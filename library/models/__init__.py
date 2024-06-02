321b176190f82f9896262d89a0c5bd0d86927e4e
vPareiko <parieiko.vlad@gmail.com>
2024-06-02 18:24:44 +0400

    Register all models to package

---
+++ library/models/__init__.py
@@ -0,0 +1,7 @@
+from library.models.author import Author, AuthorDetail
+from library.models.book import Book
+from library.models.borrow import Borrow
+from library.models.category import Category
+from library.models.event import Event, EventParticipant
+from library.models.library import Library
+from library.models.member import Member
