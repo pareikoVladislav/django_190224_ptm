18aeb045c083a3b778be7ea48a888808f7c16975
vPareiko <parieiko.vlad@gmail.com>
2024-06-05 02:21:54 +0400

    Add new field to book model

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from django.utils import timezone
 
-# Register your models here.
+from library.models.book import Book
+from library.models.member import Member
+from library.models.author import Author
+from library.models.category import Category
+from library.models.library import Library
+
+
+# class BookInline(admin.TabularInline):
+#     model = Book
+#     extra = 1
+
+class BookInline(admin.StackedInline):
+    model = Book
+    extra = 1
+
+
+class MemberAdmin(admin.ModelAdmin):
+    inlines = [BookInline]
+
+
+admin.site.register(Member, MemberAdmin)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author_id', 'publisher_id', 'published_date', 'created_at')
+
+    def update_created_at(modeladmin, request, queryset):
+        queryset.update(created_at=timezone.now())
+
+    update_created_at.short_description = "Update created_at to current time"
+
+    actions = [update_created_at]
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Library)
+class LibraryAdmin(admin.ModelAdmin):
+    ...
